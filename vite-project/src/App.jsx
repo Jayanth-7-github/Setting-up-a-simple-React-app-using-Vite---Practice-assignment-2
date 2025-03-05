e424d18319b6a2440c610a4f206e92d925a9a1de
Jayanth <kolleparajayanth@gmail.com>
2025-03-05 14:36:15 +0530

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='Box'>
+      <h1>K.Jayanth</h1>
+      <p id='p1'>Phone:9188699659</p>
+      <p id='p2'>email:jdjdndnjn@gmail.com</p>
+
+    </div>
+     
+    </>
+  )
+}
+
+export default App
